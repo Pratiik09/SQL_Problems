7f4f37b5bd2f69f9b96b5a02f641d61e5f902a88
Pratiik09 <pratikpadman01@gmail.com>
2024-04-13 08:45:12 +0530

    Rolling Avg Question

--- Other_Sources/Problems.sql
+++ Other_Sources/Problems.sql
@@ -1,3 +1,5 @@
+-- Question 1
+-- For every employee, get the start date and end date. If end date is not available, mark as NULL
 -- INPUT:
 EmployeeID	StartDate	EndDate
 1	        2023-01-01	2023-06-30
@@ -26,8 +28,6 @@ EmployeeID	startdate	enddate
 3	        2023-02-01	NULL
 4	        2023-08-01	2024-01-31
 
--- For every employee, get the start date and end date. If end date is not available, mark as NULL
-
 -- Solution:
 -- Using Window Function
 SELECT DISTINCT EmployeeID,
@@ -42,3 +42,54 @@ SELECT
 	MAX(EndDate) AS EndDate
 FROM EmployeeRecords
 GROUP BY EmployeeID
+
+
+-- Question 2
+-- Rolling Average of Last 3 Months
+-- INPUT
+user_id | purchase_amount | purchase_date
+------- | ---------------- | --------------
+1       | 10.00            | 2024-01-10
+2       | 20.00            | 2024-02-05
+3       | 30.00            | 2024-02-15
+1       | 5.00             | 2024-03-01
+2       | 15.00            | 2024-03-10
+
+-- OUTPUT
+year	month	rolling_avg
+2024	1		10.000000
+2024	2		30.000000
+2024	3		26.666667
+
+-- Create Input
+CREATE TABLE purchases (
+  user_id INT,
+  purchase_amount DECIMAL(10,2),
+  purchase_date DATE
+);
+
+INSERT INTO purchases (user_id, purchase_amount, purchase_date)
+VALUES (1, 10.00, '2024-01-10'),
+       (2, 20.00, '2024-02-05'),
+       (3, 30.00, '2024-02-15'),
+       (1, 5.00, '2024-03-01'),
+       (2, 15.00, '2024-03-10');
+
+-- Solution
+WITH monthly_revenue AS (
+	SELECT
+		YEAR(purchase_date) AS year,
+        MONTH(purchase_date) AS month,
+        SUM(purchase_amount) AS total_revenue
+	FROM purchases
+    GROUP BY YEAR(purchase_date), MONTH(purchase_date)
+)
+SELECT
+	year,
+    month,
+    AVG(total_revenue) OVER(
+    ORDER BY year, month
+    ROWS BETWEEN 2 PRECEDING AND CURRENT ROW
+    ) AS rolling_avg
+FROM monthly_revenue
+ORDER BY year, month
